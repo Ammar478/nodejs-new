10d496fd30d57f71d056010873b1e553bea7e355
Ammar Ahmed <58107422+Ammar478@users.noreply.github.com>
2023-11-10 18:13:41 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({ je: 'hellow world' })
+})
+
+const port = Number(process.env.PORT || 8080)
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Listening at http://localhost:${port}/`)
+})
